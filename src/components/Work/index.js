cd573c068078aea4d41486f2740065ee1f038299
Justin Lindsey <justinlindseylhr@gmail.com>
2021-08-18 12:59:26 -0500

    work images zoom and dim on hover, work text fades in on hover, need them to stack now

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -10,25 +10,25 @@ window.addEventListener("load", () => {
 
     const bodyBg = document.querySelector('.offScroll');
     bodyBg.classList.remove('showOnScroll');
-  
+
     const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          bodyBg.classList.add('showOnScroll');
-          return;
-        }
-  
-        bodyBg.classList.remove('showOnScroll');
-      });
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                bodyBg.classList.add('showOnScroll');
+                return;
+            }
+
+            bodyBg.classList.remove('showOnScroll');
+        });
     });
-  
+
     observer.observe(document.querySelector('.offScroll'));
-  });
+});
 
 export function Work() {
     return (
         <section className="bodySpace offScroll showOnScroll" id="work">
-                                {/* <a href="https://github.com/jjayjack/FoodiesUnite"><img src={eats} alt="" className="" /></a>
+            {/* <a href="https://github.com/jjayjack/FoodiesUnite"><img src={eats} alt="" className="" /></a>
                                 <a href="https://github.com/melanieuhrich/Rate-My-Workplace"><img src={rank} alt="" className="" /></a>
                                 <a href="https://levickane.github.io/expensetracker/"><img src={expense} alt="" className="" /></a>
                                 <a href="https://github.com/JusticeGTR/RandomPasswordGenerator"><img src={random} alt="" className="" /></a>
@@ -43,26 +43,44 @@ export function Work() {
                     <div className="container">
                         <div className="row">
                             <div className="col-sm-6">
-                                <a href="https://github.com/jjayjack/FoodiesUnite" target="_blank" rel="noreferrer nofollow"><img src={eats} alt="" className="workImage zoom" /></a>
+                                <a href="https://github.com/jjayjack/FoodiesUnite" target="_blank" rel="noreferrer nofollow">
+                                    <img src={eats} alt="" className="workImage zoom" />
+                                    <p className="imageText">A webapp made by myself and a team of 4 others. We created it as a way to search the city you live in to find any restaurants based on any search inquiery. Currently it's only for Chicago, but in the long term, we would want to add the ability to search any city. Complete with the ability to sign up for your own account, as well as log in and out. We used React, Javascript, CSS, Foursquare, Bootstrap, Mongoose, Express, Axios, and more.</p>
+                                </a>
                             </div>
                             <div className="col-sm-6">
-                                <a href="https://github.com/melanieuhrich/Rate-My-Workplace" target="_blank" rel="noreferrer nofollow"><img src={rank} alt="" className="workImage zoom" /></a>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-sm-6">
-                                <a href="https://levickane.github.io/expensetracker/" target="_blank" rel="noreferrer nofollow"><img src={expense} alt="" className="workImage zoom" /></a>
-                            </div>
-                            <div className="col-sm-6">
-                                <a href="https://github.com/JusticeGTR/RandomPasswordGenerator" target="_blank" rel="noreferrer nofollow"><img src={random} alt="" className="workImage zoom" /></a>
+                                <a href="https://github.com/melanieuhrich/Rate-My-Workplace" target="_blank" rel="noreferrer nofollow">
+                                    <img src={rank} alt="" className="workImage zoom" />
+                                    <p className="imageText">This webapp was made by myself and a team of 4 others. A user can create an account in order to find a company and read reviews on workplace safety as well as leave their own review in order to keep workplaces accountable. This project has lots of room for growth in the future. We used React, Javascript, Css, Handlebars, Express, Sequelize, bcrypt, mySql, and more.</p>
+                                </a>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-sm-6">
-                                <a href="https://github.com/JusticeGTR/EmployeeDirectory" target="_blank" rel="noreferrer nofollow"><img src={employee} alt="" className="workImage zoom" /></a>
+                                <a href="https://levickane.github.io/expensetracker/" target="_blank" rel="noreferrer nofollow">
+                                    <img src={expense} alt="" className="workImage zoom" />
+                                    <p className="imageText">A webapp made by myself and 2 others. By adding expenditures and profits, a user can keep track of their money and understand how it's being used in order to save as much as possible. We used Javascript, CSS, HTML, JQuery, Materialize, and more.</p>
+                                </a>
                             </div>
                             <div className="col-sm-6">
-                                <a href="https://github.com/JusticeGTR/FitnessTracker" target="_blank" rel="noreferrer nofollow"><img src={fitness} alt="" className="workImage zoom" /></a>
+                                <a href="https://github.com/JusticeGTR/RandomPasswordGenerator" target="_blank" rel="noreferrer nofollow">
+                                    <img src={random} alt="" className="workImage zoom" />
+                                    <p className="imageText">This webapp is a simple and clean generator for passwords. Choose the parameters with the pop up prompts and it will generate a password for you based on the specific options it is given. Made with Javascript, CSS and HTML.</p>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <a href="https://github.com/JusticeGTR/EmployeeDirectory" target="_blank" rel="noreferrer nofollow">
+                                    <img src={employee} alt="" className="workImage zoom" />
+                                    <p className="imageText">This webapp is a great way to search through a large list of people/items easily and quickly. Search results update with any character typed in the search bar. Made with Javascript, React, Bootstrap, moment, and more.</p>
+                                </a>
+                            </div>
+                            <div className="col-sm-6">
+                                <a href="https://github.com/JusticeGTR/FitnessTracker" target="_blank" rel="noreferrer nofollow">
+                                    <img src={fitness} alt="" className="workImage zoom" />
+                                    <p className="imageText">This particular webapp is used to keep track of your fitness routines. Create a workout and input everything about your session. It will then sum up all the information of each specific exercise to show you how much you've done as well as give you the last weeks worth of workouts on a graph. Made with Javascript, Express, Mongoose, CSS, and more.</p>
+                                </a>
                             </div>
                         </div>
                     </div>
