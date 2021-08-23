43c21828d6ea1b976e1f3f3ff78fe9d110764522
Justin Lindsey <justinlindseylhr@gmail.com>
2021-08-23 16:34:55 -0500

    finished image gallery css and fixed some responsive items

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -28,11 +28,11 @@ export function About() {
                         <p className="sectionTitle">About</p>
                     </div>
                     <div className="col">
-                        <img src={profile} alt="Justin with his dog, Charlotte, resting her head on his shoulder" className="offScroll showOnScroll"/>
+                        <img src={profile} alt="Justin with his dog, Charlotte, resting her head on his shoulder" className="offScroll showOnScroll responsive"/>
                     </div>
                     <div className="col">
                         <div className="card-body">
-                            <p className="card-text">I am an artist turned web developer. After studying graphic design in college and dabbling in most other mediums, I decided to try something new. A friend suggested to try out coding, and I fell into the deep end immediately. I want to build things to help others and make them smile, and in at least that small way, help develop our society into something we can all be proud of.
+                            <p className="card-text aboutText">I am an artist turned web developer. After studying graphic design in college and dabbling in many other mediums, I decided to try something new. I want to build things to help others and make them smile, and in at least that small way, help develop our society into something we can all be proud of.
                 I love traveling, reading, creating and a wide variety of other things.</p>
                         </div>
                     </div>
