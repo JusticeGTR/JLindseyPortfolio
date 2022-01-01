4c1cc4c5d22fa62ab4647b7b63c027c0fc7c2d8d
Justin Lindsey <justinlindseylhr@gmail.com>
2022-01-01 17:39:06 -0600

    updated about text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -32,7 +32,10 @@ export function About() {
         </div>
         <div className="col">
           <div className="card-body">
-            <p className="card-text aboutText">I am an artist turned web developer. I studied graphic design in college, and worked freelance afterward. Since then, most work was more customer service based with managerial experience. I draw, paint, write and read in my spare time, as well as enjoy stories of all different mediums. Someday I would like to make my own animated film. I want to build things to help others and make them smile, and in at least that small way, help develop our society into something we can all be proud of. </p>
+            <p className="card-text aboutText">I am an artist turned web developer. I studied graphic design in college, and worked freelance afterward. Since then, I discovered the joy of coding and its
+             ever evolving world. Now, I seek to bring all of my previous experience and an apetite to learn to my work. I draw, paint, write and read in my spare time, as well as enjoy stories of all 
+             different mediums. Someday I would like to make my own animated film. I want to build things to help others and make them smile, and in at least that small way, help develop our society into 
+             something we can all be proud of. </p>
           </div>
         </div>
       </div>
